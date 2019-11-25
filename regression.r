1aba6c5313946f3cba0f55074619022d97e2a600
nicky <57533559+nickyhuang007@users.noreply.github.com>
2019-11-25 20:03:16 +0800

    Add files via upload

--- regression.r
+++ regression.r
@@ -44,7 +44,18 @@ data2 <- data.frame(data[,1:2],data[,4:6],data[,8:13],e2)
 lm2 <- lm(e2~.,data = data2)
 an1 <- anova(lm2)
 
-
+###########多元加权最小二乘估计
+s <-seq(10,20,1)
+result1 <- vector(length = 9,mode = "list")
+result2 <- vector(length = 9,mode = "list")
+for (j in 1 : 9)
+{
+  w <- data[,13] ^ (-s[j])
+  lm4 <- lm(MEDV~.,weights = w,data = data4)
+  result1[[j]] <- logLik(lm4)
+  result2[[j]] <- summary(lm4)
+}
+result1
 #X <- data.frame()
 
 #cor1 <- cor.test(X,abse,alternative="two.sided",method="spearman",conf.level=0.95)
